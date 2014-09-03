5d9cb746c084378b8b643dd9814fcdc7e5f18c6a
seibertm <seibertmle@seibertms-MacBook-Pro.local>
2014-09-03 14:15:08 +0100

    updated LeLoggerTests.php to allow for newly added variables

--- unit_tests/LeLoggerTests.php
+++ unit_tests/LeLoggerTests.php
@@ -28,57 +28,64 @@
 
 		public function testAllParameters()
 		{
-			$this->assertInstanceOf('LeLogger', LeLogger::getLogger('token', false, false, LOG_DEBUG));
+			$this->assertInstanceOf('LeLogger', LeLogger::getLogger('token', "", false, false, LOG_DEBUG, false, 10000, "", "", false));
+
 		}
 
 		public function testIsPersistent()
 		{
-			$log = LeLogger::getLogger('token',false,true,LOG_DEBUG);
+			$log = LeLogger::getLogger('token', "", false, false, LOG_DEBUG, false, 10000, "", "", false);
+
 
 			$this->assertFalse($log->isPersistent());
 
 			$this->tearDown();
 
-			$log = LeLogger::getLogger('token',true,true,LOG_DEBUG);
+			$log = LeLogger::getLogger('token', "", false, false, LOG_DEBUG, false, 10000, "", "", false);
 
 			$this->assertTrue($log->isPersistent());
 		}
 
 		public function testIsTLS()
 		{
-			$log = LeLogger::getLogger('token',false,false,LOG_DEBUG);
+			$log = LeLogger::getLogger('token', "", false, false, LOG_DEBUG, false, 10000, "", "", false);
+
 
 			$this->assertFalse($log->isTLS());
 
 			$this->tearDown();
 
-			$log = LeLogger::getLogger('token',true,true,LOG_DEBUG);
+			$log = LeLogger::getLogger('token', "", false, false, LOG_DEBUG, false, 10000, "", "", false);
+
 
 			$this->assertTrue($log->isTLS());
 		}
 
 		public function testGetPort()
 		{
-			$log = LeLogger::getLogger('token', true, false, LOG_DEBUG);
+
+			$log = LeLogger::getLogger('token', "", false, false, LOG_DEBUG, false, 10000, "", "", false);
+
 
 			$this->assertEquals(10000, $log->getPort());
 
 			$this->tearDown();
 
-			$log = LeLogger::getLogger('token', true, true, LOG_DEBUG);
+			$log = LeLogger::getLogger('token', "", false, false, LOG_DEBUG, false, 10000, "", "", false);
 
 			$this->assertEquals(20000, $log->getPort());
 		}
 
 		public function testGetAddress()
 		{
-			$log = LeLogger::getLogger('token', true, false, LOG_DEBUG);
+			$log = LeLogger::getLogger('token', "", false, false, LOG_DEBUG, false, 10000, "", "", false);
 
 			$this->assertEquals('tcp://api.logentries.com', $log->getAddress());
 
 			$this->tearDown();
 
-			$log = LeLogger::getLogger('token', true, true, LOG_DEBUG);
+			$log = LeLogger::getLogger('token', "", false, false, LOG_DEBUG, false, 10000, "", "", false);
+
 
 			$this->assertEquals('tls://api.logentries.com', $log->getAddress());
 		}
